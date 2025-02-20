91020bbffd1e4aa8bf34168630beee239b51ffa6
jakednich <jakenichols57@gmail.com>
2025-02-19 19:55:36 -0600

    Completed exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    let value = value
+    let array = array
+    array = array.filter(function(item) {
+        return item !== value
+    })
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    if (typeof(arg1) !== 'number' || 
+        typeof(arg2) !== 'number' ||
+        !Number.isInteger(arg1) ||
+        !Number.isInteger(arg2) ||
+        arg1 < 0 ||
+        arg2 < 0) { 
+        return 'ERROR';
+    } 
 
+    let sum = 0;
+    let start = Math.min(arg1, arg2);
+    let end = Math.max(arg1, arg2);
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    } return sum;
 };
+
+
+// TODO
+// function that takes two integers as arguments
+// takes every number between the arguments and sums them including args
 
 // Do not edit below this line
 module.exports = sumAll;
